585305e6d716ff2d0ebce0c5c3ae6801c30cb9d9
Piyushraj Bista <piyushrajbista@outlook.com>
2023-03-23 18:04:23 +0545

    fix: updated to utilize new json field

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 
-
-
+ 
+// make a ajax get request to user json on page load
 let userData
 $(document).ready(function(){
 
     $.ajax({url:"user.json", success:function(result){
-
+        //store json data to userData variable
         userData=result
         console.log(userData)
     }})
 })
 
-var input
 
+var input
+// function on click 
 $("#search").click(function(){
     input= $("#input").val()
     for(let i=0;i< userData.length;i++){
         if(input === userData[i].firstname)
         {
             $("#uid").text(userData[i].id)
+
             if($("#fname").is(':checked') == true){
                 $("#ufirstname").text(userData[i].firstname)
             }
@@ -28,8 +30,8 @@ $("#search").click(function(){
             if($("#address").is(':checked') == true){
                 $("#uaddress").text(userData[i].address)
             }
-            if($("#phone").is(':checked') == true){
-                $("#uphone").text(userData[i].phone)
+            if($("#num").is(':checked') == true){
+                $("#userphone").text(userData[i].contact)
             }
         }
     }
